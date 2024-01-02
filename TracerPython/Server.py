ed746518c7ff333968aaf56aeabe504ca7b43f62
ivansavrikov <yeahsavrikov@gmail.com>
2024-01-03 00:14:22 +0700

    feat: add fastapi

---
+++ TracerPython/Server.py
@@ -0,0 +1,37 @@
+from core.BuilderSVG import BuilderSVG
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import FileResponse
+from fastapi.middleware.cors import CORSMiddleware
+from PIL import Image
+from io import BytesIO
+from fastapi.responses import StreamingResponse
+
+#uvicorn Server:app --reload
+app = FastAPI()
+
+TEST_FOLDER = './ResourcesForTesting/TestFastAPI/'
+new_image_path = f"{TEST_FOLDER}default.svg"
+
+# CORS (Cross-Origin Resource Sharing) для работы с внешним frontend
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Разрешить все origin (необходимо настроить для безопасности в реальном приложении)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.post("/tracer/")
+async def trace(file: UploadFile = File(...)):
+    contents = await file.read()
+    
+    image = Image.open(BytesIO(contents))
+    builder = BuilderSVG()
+    svg_code = builder.raster_to_svg(image)
+    
+    svg_data = BytesIO()
+    svg_data.write(svg_code.encode("utf-8"))
+    svg_data.seek(0)
+
+    # Возвращение SVG-файла в ответе
+    return StreamingResponse(svg_data, media_type="image/svg+xml", headers={"Content-Disposition": f"attachment; filename={file.filename}.svg"})
